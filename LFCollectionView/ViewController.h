514e03c595591a8a853a499d0eb8eeddfcc50ab8
young1park <lossefanya@gmail.com>
2015-08-20 00:22:46 +0200

    Initial Commit

---
+++ LFCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFCollectionView
+//
+//  Created by Young One Park on 2015. 8. 20..
+//  Copyright (c) 2015년 young1park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LFCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LFCollectionView
+//
+//  Created by Young One Park on 2015. 8. 20..
+//  Copyright (c) 2015년 young1park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
